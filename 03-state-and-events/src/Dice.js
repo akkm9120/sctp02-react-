b3be99ee7d02ffbc5546bc4224aefe4d832b5c27
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-03-22 08:33:30 +0000

    conditioncal Rendering

--- 03-state-and-events/src/Dice.js
+++ 03-state-and-events/src/Dice.js
@@ -6,10 +6,12 @@ function Dice(){
        setDice(Math.floor(1+Math.random() * 6))
     }
     return <div style={{"padding":"20px",
-                        "border":"2px solid green"}} 
+                        "border":"2px solid green",
+                        "color": "#fffff",
+                        "backgroundColor": dice % 2 === 0 ? "green" : "red" }} 
                         onClick={Random}>
 
-        The number on your Dice is {dice}. { dice > 4 ? <p>Big</p>: null }
+        The number on your Dice is {dice} 
     </div>
 }
 
