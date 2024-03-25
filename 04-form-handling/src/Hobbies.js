226e4a092a9fadcb657f5e2ec7596bbf808f1b68
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-03-25 08:20:55 +0000

    delete and add to the checkboxs

--- 04-form-handling/src/Hobbies.js
+++ 04-form-handling/src/Hobbies.js
@@ -1,14 +1,32 @@
 import { useState } from "react";
 export default function Hobbies() {
-  const [hobbies, setHobbies] = useState([]);
+  const [hobbies, setHobbies] = useState(["reading"]);
 
   // more code here
   const handleHobbiesChange = (event) => {
+    if(event.checked){
     const modified = [...hobbies, event.target.value];
     setHobbies(modified);
+    }else {
+        if (event.target.checked) {
+            const modified = [...hobbies, event.target.value];
+            setHobbies(modified);
+          } else {
+            const cloned = hobbies.slice();
+            let indexToRemove = cloned.findIndex(function(e) {
+              return e === event.target.value;
+            });
+          
+            if (indexToRemove !== -1) {
+              cloned.splice(indexToRemove, 1);
+            }
+            setHobbies(cloned);
+          }         
+    }
   }
 
   return (
+
       <div className="App">
           <h1>Hobbies</h1>
           <h2>Selected Hobbies:</h2>
